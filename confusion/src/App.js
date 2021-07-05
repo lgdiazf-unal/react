983152e1be74c26f58f3116928119b7878c4b15d
Your Name <you@example.com>
2021-07-04 18:59:56 -0700

    part 2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,20 +1,34 @@
 //import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Menu from './components/MenuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import {DISHES} from './shared/dishes'
 
 
-function App() {
-  return (
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state ={
+      dishes : DISHES
+    }
+  }
+
+  render(){
+    return  (
       <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
   );
+
+  }
+  
 }
 
 export default App;
