1f3fe42767275ea22fa302e993e9d059a738dfbc
Your Name <you@example.com>
2021-07-07 20:35:36 -0700

    task 2

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -10,19 +10,62 @@ class Dishdetail extends Component{
     }
 
 
-    render(){
+    renderComments(){
 
-        if (this.props.dish != null){
-            return(
-                <Card key={this.props.dish.id}>
-                    <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-                    <CardBody>
-                      <CardTitle>{this.props.dish.name}</CardTitle>
-                      <CardText>{this.props.dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
+        
+
+        let comments = <div></div>
+        
+        if (this.props.dish.comments != null){
             
+            comments = this.props.dish.comments.map(comment=>{
+                let  mydate = new Date(comment.date)
+                var month = mydate.getMonth(); // month (in integer 0-11)
+                var year = mydate.getFullYear(); // year
+
+                console.log(month,year)
+                return (
+                    <li>
+                        <li>{comment.comment}</li>
+                        <li className="m-3">-- { comment.author} , {month}  </li>
+                    </li>
+                    
+                );
+            })
+        }
+        
+
+        return comments
+    }
+
+
+    render(){
+        if (this.props.dish != null){
+            
+            return(
+                <div >
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <Card key={this.props.dish.id}>
+                            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                            <CardBody>
+                            <CardTitle>{this.props.dish.name}</CardTitle>
+                            <CardText>{this.props.dish.description}</CardText>
+                            </CardBody>
+                            </Card>
+                        </div>
+
+                        <div className="col-12 col-md-5 m-1">
+                            <h4>Comments</h4>
+                            <ul class = "list-unstyled">
+
+                                {this.renderComments()}
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            );
         }
             
         else{
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -16,6 +16,7 @@ class Menu extends Component {
     }
 
 
+
     render() {
       const menu = this.props.dishes.map((dish) => {
           return (
@@ -35,12 +36,8 @@ class Menu extends Component {
           <div className="container">
               <div className="row">
                   {menu}
-              </div>
-              <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                  <Dishdetail dish={this.state.selectedDish} />
-                </div>
-              </div>
+              </div>         
+                <Dishdetail dish={this.state.selectedDish} />
           </div>
       );
     }
