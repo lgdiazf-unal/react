a94a984e42aa4e4ad406d30f2ab8d95b5d2d0177
Your Name <you@example.com>
2021-07-08 18:54:53 -0700

    week 1

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -9,25 +9,35 @@ class Dishdetail extends Component{
         };
     }
 
+    renderDish(dish){
+        return(
+            <Card key={dish.id}>
+                <CardImg top src={dish.image} alt={this.props.dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        )
+    }
+
 
     renderComments(){
-
-        
-
         let comments = <div></div>
         
         if (this.props.dish.comments != null){
             
             comments = this.props.dish.comments.map(comment=>{
                 let  mydate = new Date(comment.date)
-                var month = mydate.getMonth(); // month (in integer 0-11)
-                var year = mydate.getFullYear(); // year
+                var month = mydate.toLocaleString('en-us', { month: 'short' }); 
+                var year = mydate.getFullYear(); 
+                var day = mydate.getDay();
 
                 console.log(month,year)
                 return (
                     <li>
                         <li>{comment.comment}</li>
-                        <li className="m-3">-- { comment.author} , {month}  </li>
+                        <li className="m-3">-- { comment.author} , {month} {day} , {year}   </li>
                     </li>
                     
                 );
@@ -46,13 +56,7 @@ class Dishdetail extends Component{
                 <div >
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
-                            <Card key={this.props.dish.id}>
-                            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-                            <CardBody>
-                            <CardTitle>{this.props.dish.name}</CardTitle>
-                            <CardText>{this.props.dish.description}</CardText>
-                            </CardBody>
-                            </Card>
+                            {this.renderDish(this.props.dish)}
                         </div>
 
                         <div className="col-12 col-md-5 m-1">
